a376c99bb51ec1f7b58725b8277d776e32bbb619
nata1976elya1976@gmail.com <nata1976elya1976@gmail.com>
2020-06-10 19:36:58 +0300

    Добавьте файлы проекта.

---
+++ 2 задача/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_задача
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] a = new int[10];
+
+            Random r = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+                a[i] = r.Next(-100, 100);
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.Write(" " + a[i]);
+            }
+            Console.WriteLine();
+            int p = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (a[i] > 0)
+                {
+                    p *= a[i];
+                }
+            }
+            Console.WriteLine("произведение положительных элементов = "+p);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2 задача/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("2 задача")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2 задача")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b21686ab-d90b-4154-8c71-a09942c3d84a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
